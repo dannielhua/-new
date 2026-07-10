db32e41c1457d602017bd7ffeae2edd777c64a6f
dannielhua <46063146+dannielhua@users.noreply.github.com>
2026-07-10 08:57:42 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = 'https://your-project.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOi...';  // 替换成你自己的
+const SUPABASE_URL = 'https://ervhqbfelryohfvyuaok.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVydmhxYmZlbHJ5b2hmdnl1YW9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMTA5MTQsImV4cCI6MjA5ODg4NjkxNH0.z-FdF47zHB3ai0f1VylUgVbxWUwBZF7I0oY8NAwh2gw';  // 替换成你自己的
 const mySupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
